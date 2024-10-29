cce629cc5a6db04f600fcb94a6f1a08104d6ac32
Leon Dore <leon.dore@gmail.com>
2024-10-28 23:09:53 -0400

    begin working on config

---
+++ config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import "time"
+
+type Conf struct {
+	Server ConfServer
+	DB     ConfDB
+}
+
+type ConfServer struct {
+	Port         int           `env:"SERVER_PORT,required"`
+	TimeoutRead  time.Duration `env:"SERVER_TIMEOUT_READ,required"`
+	TimeoutWrite time.Duration `env:"SERVER_TIMEOUT_WRITE,required"`
+	TimeoutIdle  time.Duration `env:"SERVER_TIMEOUT_IDLE,required"`
+	Debug        bool          `env:"SERVER_DEBUG, required"`
+}
+
+type ConfDB struct {
+	Host     string `env:"DB_HOST,required"`
+	Port     int    `env:"DB_PORT,required"`
+	Username string `env:"DB_USER,required"`
+	Password string `env:"DB_PASS, required"`
+	DBName   string `env:"DB_NAME, required"`
+	Debug    bool   `env:"DB_DEFUG, requierd"`
+}
